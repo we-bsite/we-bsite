f17657ab106a1f9148d1ed05b59d825f9bdb69a3
Spencer Chang <spencerc99@gmail.com>
2022-12-25 10:14:25 -0600

    update with simple landing page

--- src/App.tsx
+++ src/App.tsx
@@ -26,15 +26,43 @@ function App() {
   ]);
 
   return (
-    <div className="App">
-      <h1>(we)bsite</h1>
-      {/* TODO: add texture to desk background, make it grainy */}
-      <div id="desk">
+    <>
+      <div className="App">
+        <h1>(we)bsite</h1>
+        <p>
+          <em>(we)bsite</em> is a living collection of internet dreams from
+          people like you, inhabitants of the internet. It aims to create space
+          to hold, show, and uplift everyday visions and hopes for the internet.
+        </p>
+        <p>
+          Despite all the time we spend on the internet, most of us are passive
+          consumers of content and interfaces. Our standard digital environments
+          do not offer let alone encourage us to imagine and create alternative
+          futures for the internet. What if everyone who used the internet was
+          encouraged to dream about it? What would it look like if we
+          collectively imagine and build a better internet together?
+        </p>
+        <p>
+          If you have an internet dream you would like to contribute and/or want
+          to help us cultivate this space, please{" "}
+          <a href="mailto:spencerc99@gmail.com,j.zhao2k19@gmail.com&subject=(we)bsite dreams">
+            reach out to us
+          </a>
+          .
+        </p>
+        {/* TODO: add texture to desk background, make it grainy */}
+        {/* <div id="desk">
         {letters.map((letter) => (
           <LetterView letter={letter} />
         ))}
+      </div> */}
       </div>
-    </div>
+      <footer>
+        (we)bsite is a project by{" "}
+        <a href="https://spencerchang.me">Spencer Chang</a> and{" "}
+        <a href="https://jzhao.xyz">Jacky Zhao</a>.
+      </footer>
+    </>
   );
 }
 
