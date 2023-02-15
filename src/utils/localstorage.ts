ddc424e737b0dffcba69eff9bbd1fe5eb92bdc98
Spencer Chang <spencerc99@gmail.com>
2023-02-14 18:21:52 -0800

    jk really fix it

--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -12,18 +12,24 @@ export function getLocalStorageItem<T>(key: string): T | null {
 
 // shoutout to my boy josh comeau for this hook template.
 // https://www.joshwcomeau.com/react/persisting-react-state-in-localstorage/
+// https://dev.to/joshwcomeau/comment/m114
 export function useStickyState<T>(
   localStorageId: string,
   defaultValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [value, setValue] = React.useState<T>(() => {
+  const [value, setValue] = React.useState<T>(defaultValue);
+  // Must be done here vs. in `setState` to handle `localStorage`
+  // not defined at compile-time on server.
+  React.useEffect(() => {
     const stickyValue = getLocalStorageItem<T>(localStorageId);
-    return stickyValue !== null ? stickyValue : defaultValue;
-  });
+    if (stickyValue !== null) {
+      setValue(stickyValue);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [localStorageId]);
 
   React.useEffect(() => {
     localStorage.setItem(localStorageId, JSON.stringify(value));
   }, [localStorageId, value]);
-
   return [value, setValue];
 }
