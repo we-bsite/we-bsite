90944bd8978537b878d43a21046e74aa7353704c
Spencer Chang <spencerc99@gmail.com>
2023-02-26 13:07:29 -0800

    fix ts

--- src/utils/yjs.ts
+++ src/utils/yjs.ts
@@ -3,26 +3,38 @@ import { WebrtcProvider } from "y-webrtc";
 import Y from "yjs";
 import { StartAwarenessFunction } from "zustand-yjs";
 import { YJS_ROOM } from "../constants";
-import { DefaultPersistedUserLetterContext, PersistedUserLetterContextInfo, UserContextStorageId } from "../context/UserLetterContext";
+import {
+  DefaultPersistedUserLetterContext,
+  PersistedUserLetterContextInfo,
+  UserContextStorageId,
+} from "../context/UserLetterContext";
 import { getLocalStorageItem } from "./localstorage";
 
 export const connectDoc = (
   doc: Y.Doc,
   startAwareness: StartAwarenessFunction
 ) => {
-  const ctx = getLocalStorageItem<PersistedUserLetterContextInfo>(UserContextStorageId) ?? DefaultPersistedUserLetterContext
-  const color = ctx.color
-  console.log(`Connecting to the internet as ${color}... ${doc.guid} initialized`);
+  const ctx =
+    getLocalStorageItem<PersistedUserLetterContextInfo>(UserContextStorageId) ??
+    DefaultPersistedUserLetterContext;
+  const color = ctx.color;
+  console.log(
+    `Connecting to the internet as ${color}... ${doc.guid} initialized`
+  );
 
   // Hack to get around server-side rendering build
   if (typeof window === "undefined") {
-    return () => { };
+    return () => {};
   }
 
-  const stopCursorChatCallback = initCursorChat("(we)bsite", { yDoc: doc, color })
+  // @ts-ignore
+  const stopCursorChatCallback = initCursorChat("(we)bsite", {
+    yDoc: doc,
+    color,
+  });
   // @ts-ignore
   const provider = new WebrtcProvider(YJS_ROOM, doc, {
-    signaling: ['wss://signalling-2u32im64eq-uc.a.run.app:4444'],
+    signaling: ["wss://signalling-2u32im64eq-uc.a.run.app:4444"],
   });
   const stopAwarenessCallback = startAwareness(provider);
   return () => {
